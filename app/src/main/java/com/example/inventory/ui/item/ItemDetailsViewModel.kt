9a964ad761dc182ab967b47e40a738a60794106f
CheoR <cheo.roman+github@gmail.com>
2023-10-03 17:06:53 -0500

    build(item): screen viewModels. They needed screen destination objects so created after screens.

---
+++ app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.inventory.ui.item
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.inventory.data.ItemsRepository
+
+/**
+ * ViewModel to retrieve, update and delete item from [ItemsRepository]'s data source.
+ */
+class ItemDetailsViewModel(
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val itemId: Int = checkNotNull(savedStateHandle[ItemDetailsDestination.itemIdArg])
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+/**
+ * ItemDetailsScreen UI state
+ */
+data class ItemDetailsUiState(
+    val outOfStock: Boolean = true,
+    val itemDetails: ItemDetails = ItemDetails()
+)
