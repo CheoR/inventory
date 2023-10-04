54d2ef4692e8ebd182c0cd66348f7cb22a6e4846
CheoR <cheo.roman+github@gmail.com>
2023-10-04 15:10:36 -0500

    build(data): repository and offline repository provide CRUD entities from db. Interfaces map to DAO implementation.

--- app/src/main/java/com/example/inventory/data/ItemsRespository.kt
+++ app/src/main/java/com/example/inventory/data/ItemsRespository.kt
@@ -1,6 +1,33 @@
 package com.example.inventory.data
 
+import kotlinx.coroutines.flow.Flow
+
 /**
- * Repository that provides insert, update, delete, and retrieve of [Item] from given data source.
+ * Repository provides insert, update, delete, and retrieve of [Item] from given data source.
  */
-interface ItemsRepository
+interface ItemsRepository {
+    /**
+     * Retrieve all items from given data source.
+     */
+    fun getAllItemsStream(): Flow<List<Item>>
+
+    /**
+     * Retrieve item from given data source that matches with [id].
+     */
+    fun getItemStream(id: Int): Flow<Item?>
+
+    /**
+     * Insert item into data source
+     */
+    suspend fun insertItem(item: Item)
+
+    /**
+     * Delete item from data source
+     */
+    suspend fun deleteItem(item: Item)
+
+    /**
+     * Update item in data source
+     */
+    suspend fun updateItem(item: Item)
+}
