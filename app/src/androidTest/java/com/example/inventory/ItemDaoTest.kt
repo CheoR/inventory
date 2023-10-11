db1229a5ce8961dac69262712da884cef898f6c7
CheoR <cheo.roman+github@gmail.com>
2023-10-11 08:50:03 -0500

    test(Dao): delete from db functionality

--- app/src/androidTest/java/com/example/inventory/ItemDaoTest.kt
+++ app/src/androidTest/java/com/example/inventory/ItemDaoTest.kt
@@ -16,6 +16,7 @@ import java.io.IOException
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 @RunWith(AndroidJUnit4::class)
@@ -89,5 +90,18 @@ class ItemDaoTest {
 
         assertEquals(allItems[0], Item(1, "Apples", 15.0, 25))
         assertEquals(allItems[1], Item(2, "Bananas", 5.0, 50))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun daoDeleteItems_deletesAllItemsFromDB() = runBlocking {
+        addTwoItemsToDb()
+
+        itemDao.delete(item1)
+        itemDao.delete(item2)
+
+        val allItems = itemDao.getAllItems().first()
+
+        assertTrue(allItems.isEmpty())
     }
 }
